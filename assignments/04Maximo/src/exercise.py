8a89ec2b15957980d9f7dbeffda2c53b6d144247
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-08-23 14:40:45 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+c = float(input("Ingresa el tercer número: "))
+ 
+if (a > b) and (a > c):
+   g = a
+elif (b > a) and (b > c):
+   g = b
+else:
+   g = c
+ 
+print(str(int(g)))
     pass
 
 
