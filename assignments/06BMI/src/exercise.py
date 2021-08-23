4307965fd9aa4844dd21512e5d42ff724c54446c
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-08-23 16:52:52 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,27 @@
 def main():
     #escribe tu código abajo de esta línea
+    p = float(input("Peso en Kg: "))
+a = float(input("Altura en metros: "))
+
+i = p/(a**2)
+ 
+if (0 > i < 20):
+   print('PESO BAJO')
+
+elif (20 <= i <25):
+    print('NORMAL')
+
+elif (25 <= i <30):
+    print('SOBREPESO')
+
+elif (30 <= i <40):
+    print('OBESIDAD')
+
+elif (40 < i):
+    print('OBESIDAD MORBIDA')
+
+else:
+    print('Revisa tus datos, alguno de ellos es erróneo.')
     pass
 if __name__=='__main__':
     main()
