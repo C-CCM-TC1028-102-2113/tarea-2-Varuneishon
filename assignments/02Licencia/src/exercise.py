ff443d18b21e9466a2cb339fa3a938045fbc6a73
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-08-23 14:26:20 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,22 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    e = int(input ("DIngresa tu edad: " ))
+
+
+if e>=19:
+    print("¿Tienes identificación oficial? (s/n): ")
+    i = str(input())
+
+else:
+    print("No cumples requisitos")
+
+
+if (i == 'n') or (i == 's'):
+    print("Trámite de licencia concedido")
+
+else:
+    print("TNo cumples requisitos")
     pass
 
 
