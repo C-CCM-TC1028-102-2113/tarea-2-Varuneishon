c0b2ed0455ae712efe0bbf1249db99343e764591
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-08-23 14:08:07 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,6 +1,19 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    n = int(input ("Dame un número: " ))
+
+
+if n==0:
+    print("Es cero ")
+
+elif n>0:
+    print("Es positivo")
+
+
+else:
+    n<0
+    print("Els negativo ")
     pass
     
 
